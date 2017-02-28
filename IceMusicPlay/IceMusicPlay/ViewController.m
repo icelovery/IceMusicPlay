77b2e539d3cbcf73011236d68b5cf51f586b4f8e
songlinlin <songlinlin@lifesay.cn>
2017-02-28 16:53:05 +0800

    New button

--- IceMusicPlay/IceMusicPlay/ViewController.m
+++ IceMusicPlay/IceMusicPlay/ViewController.m
@@ -17,8 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createUI];
 }
 
+-(void)createUI{
+    UIButton * btn = [[UIButton alloc]initWithFrame:CGRectMake(30, 200, 150, 50)];
+    [btn setTitle:@"test" forState:UIControlStateNormal];
+    btn.backgroundColor =[UIColor blueColor];
+    [btn addTarget:self action:@selector(testClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+
+}
+
+-(void)testClick:(UIButton *)sender{
+    NSLog(@"点击按钮");
+    UIWindow * window = [[UIWindow alloc]init];
+    UIView * view =[[UIView alloc]initWithFrame:CGRectMake(50, 150, 150, 150)];
+    view.backgroundColor= [UIColor redColor];
+    [window  addSubview: view];
+    [self.view addSubview:window];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
