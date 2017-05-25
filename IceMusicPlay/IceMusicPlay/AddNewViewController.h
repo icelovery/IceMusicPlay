4ff3f473e7f8b9244b4393b4433c5705a4b26e87
songlinlin <songlinlin@lifesay.cn>
2017-05-25 10:27:26 +0800

    add new

---
+++ IceMusicPlay/IceMusicPlay/AddNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddNewViewController.h
+//  IceMusicPlay
+//
+//  Created by songlin on 2017/5/25.
+//  Copyright © 2017年 songlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNewViewController : UIViewController
+
+@end
